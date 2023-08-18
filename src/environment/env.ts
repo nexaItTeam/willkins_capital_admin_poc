a436ff31803c4b3e66175c5559e6137d586720e3
nexaItTeam <itsupport@arctechz.com>
2023-08-18 14:59:37 +0530

    change link

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,7 +1,7 @@
-export const environment ={
-    PROPERTY_ENDPOINT:"https://nexa-capital.onrender.com/api/property/",
-ENQUIRY_ENDPOINT:"https://nexa-capital.onrender.com/api/enquery/",
-BLOG_ENDPOINT:"https://nexa-capital.onrender.com/api/blog/",
-USER_ENDPOINT:"https://nexa-capital.onrender.com/api/user/",
-CLIENT_ENDPOINT:"https://nexa-capital.onrender.com/api/client/"
+export const environment = {
+    PROPERTY_ENDPOINT: "https://wellkin-backend-dev.azurewebsites.net/api/property/",
+    ENQUIRY_ENDPOINT: "https://wellkin-backend-dev.azurewebsites.net/api/enquery/",
+    BLOG_ENDPOINT: "https://wellkin-backend-dev.azurewebsites.net/api/blog/",
+    USER_ENDPOINT: "https://wellkin-backend-dev.azurewebsites.net/api/user/",
+    CLIENT_ENDPOINT: "https://wellkin-backend-dev.azurewebsites.net/api/client/"
 }
